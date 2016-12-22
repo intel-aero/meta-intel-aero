0de47f8431c9fd484ac8d1555b6d5e48aa043436
Mandeep Bansal <mandeep.bansal@intel.com>
2016-12-21 17:54:13 -0800

    initscripts: make bios_version mutable
    BIOS version is not updated correctly in a scenario,
    where only BIOS is updated. Hence making bios_version
    mutable prior to storing the version
    
    Signed-off-by: Pranav Tipnis <pranav.tipnis@intel.com>
    Signed-off-by: Mandeep Bansal <mandeep.bansal@intel.com>

--- recipes-core/initscripts/initscripts/build_version.sh
+++ recipes-core/initscripts/initscripts/build_version.sh
@@ -3,6 +3,7 @@
 # a) Read and store BIOS version
 # b) Make os_version file immutable
 
+chattr -i /etc/bios_version
 dmesg | grep DMI > /etc/bios_version
 chmod 444 /etc/bios_version
 chattr +i /etc/bios_version
