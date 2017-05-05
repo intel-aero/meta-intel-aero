034d1f9efc8c910629a3b3761d080674a22c3bed
Lucas De Marchi <lucas.demarchi@intel.com>
2017-05-05 13:39:52 -0700

    Remove recipes migrated to meta-intel-aero-base

--- recipes-core/initscripts/initscripts/build_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This script will:
-# a) Read and store BIOS version
-
-chattr -i /etc/bios_version
-dmesg | grep DMI > /etc/bios_version
-chmod 444 /etc/bios_version
-chattr +i /etc/bios_version
